34697efbc2c3a5bdb57fbd310a6a7a01e9bf9b33
LarturCG <leonardoarturo360@gmail.com>
2025-06-22 17:10:59 -0500

    ESTA SI

--- backend_excel.py
+++ backend_excel.py
@@ -100,7 +100,7 @@ def generar_excel():
         #  columnas
         ws.column_dimensions['A'].width = 7
         ws.column_dimensions['B'].width = 9  
-        ws.column_dimensions['C'].width = 10  
+        ws.column_dimensions['C'].width = 11  
         ws.column_dimensions['D'].width = 19  
         ws.column_dimensions['E'].width = 14  
         ws.column_dimensions['F'].width = 13  
